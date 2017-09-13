9ad93b6e45094a4c6c622250bcee2faaf2ec454d
Mihail Bodrov <connormiha1@gmail.com>
2017-09-13 03:04:59 +0300

    Add flow types, travis :wrench:

---
+++ __tests__/flow-types/index.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+const block = require('../../src/bem-css-modules.js');
+
+block({foo: 1});
+block({foo: 1})('element');
+block({foo: 1})();
+block({foo: 1})(null);
+block({foo: 1})(null, {foo: true, foo2: '', foo3: 1});
+block({foo: 1})(null, null, {foo: true, foo1: false});
